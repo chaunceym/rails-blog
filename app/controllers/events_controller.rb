626f82e9374e3361279bc512b1ef861f75019246
chauncey <18322276773@163.com>
2020-05-24 11:13:08 +0800

    finish the show of CURD‘s message

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :set_event, :only => [:show,:edit,:update,:destroy]
   # GET /events/index
   # Get /events
   def index
@@ -11,36 +12,33 @@ class EventsController < ApplicationController
   #POST /events/create
   def create
     @event = Event.new(event_params) 
-    @event.save
-    # 重定向，提交资料后
-    redirect_to :action => :index
-  end
-
-  # /events/show/:id
-  def show
-    @event = Event.find(params[:id])
-  end
-
-  # /events/edit/:id
-  def edit
-    @event = Event.find(params[:id])
-  end
-  # /events/update/:id
+    if @event.save
+      flash[:notice] = "新增成功"
+      # 重定向，提交资料后
+      redirect_to :action => :index
+    else
+      render :action => :new 
+    end  
+  end # /events/show/:id def show @page_title = @event.name end # /events/edit/:id def edit end # /events/update/:id
   def update
-    @event = Event.find(params[:id])
-
-    @event.update(event_params)
-    redirect_to :action => :show ,:id => @event
+    if @event.update(event_params)
+      flash[:notice] = "更新成功"
+      redirect_to :action => :show ,:id => @event
+    else
+      render :action => :edit
+    end
   end
   # /events/destroy/:id
   def destroy
-    @event = Event.find(params[:id])
-
     @event.destroy
+      flash[:alert] = "删除成功"
     redirect_to :action => :index
   end
   # 因为表单提交有安全隐患，定制一个私有方法来控制表单提交的条件
   private 
+  def set_event
+    @event = Event.find(params[:id])
+  end
   def event_params
     params.require(:event).permit(:name,:description)
   end
